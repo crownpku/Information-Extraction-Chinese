9a12ed8658250bcbb51d295d5ee6a8987f761963
crownpku <crownpku@gmail.com>
2017-08-09 17:58:06 +0800

    remove some files

--- relation_rules/.ipynb_checkpoints/process_mysql-checkpoint.py
+++
@@ -1,7 +0,0 @@
-infile = open('BNC_SP_Final')
-outfile = open('bnc_sp_final_mysql', 'w')
-for n, line in enumerate(infile):
-    line = line.strip().split(',', 1)
-    title = line[0]
-    content = line[1]
-    print >> outfile, str(n) + '' + 'NA' + ''
