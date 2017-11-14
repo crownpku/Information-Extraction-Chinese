1aca2150ece59f1796c91cf575c1274bf7316d1a
crownpku <crownpku@gmail.com>
2017-11-14 09:35:32 +0800

    removed unfinished coref part

--- Coref_E2E/README.md
+++
@@ -1,4 +0,0 @@
-# End to End Neural Chinese Coreference Resolution
-
-
-## UNDER HEAVY DEVELOPMENT
--- Coref_E2E/setup_pretrained.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -O http://appositive.cs.washington.edu/resources/coref/char_vocab.english.txt
-
-curl -O http://appositive.cs.washington.edu/resources/coref/final.tgz
-mkdir -p logs
-tar -xzvf final.tgz -C logs
-rm final.tgz
